84deb5a8fb152d76cb80bc096f8f2d8c7906b892
Benjamin Mueller <benjamin.mueller@zib.de>
2016-08-12 15:13:03 +0200

    add examples

---
+++ examples/Linear.java
@@ -0,0 +1,47 @@
+import jscip.*;
+
+/** Example how to create a problem with linear constraints. */
+public class Linear
+{
+   public static void main(String args[])
+   {
+      // load generated C-library
+      System.loadLibrary("jni");
+
+      Scip scip = new Scip();
+
+      // set up data structures of SCIP
+      scip.create("LinearExample");
+
+      // create variables (also adds variables to SCIP)
+      Variable x = scip.createVar("x", 2.0, 3.0, 1.0, SCIP_Vartype.SCIP_VARTYPE_CONTINUOUS);
+      Variable y = scip.createVar("y", 0.0, scip.infinity(), -3.0, SCIP_Vartype.SCIP_VARTYPE_INTEGER);
+ 	
+      // create a linear constraint
+      Variable[] vars = {x, y};
+      double[] vals = {1.0, 2.0};
+      Constraint lincons = scip.createConsLinear("lincons", vars, vals, -scip.infinity(), 10.0);
+     
+      // add constraint to SCIP
+      scip.addCons(lincons);
+      
+      // release constraint (if not needed anymore)
+      scip.releaseCons(lincons);
+      
+      // solve problem
+      scip.solve();
+      
+      // print all solutions
+      Solution[] allsols = scip.getSols();
+     
+      for( int s = 0; allsols != null && s < allsols.length; ++s )
+         System.out.println("solution (x,y) = (" + scip.getSolVal(allsols[s], x) + ", " + scip.getSolVal(allsols[s], y) + ") with objective value " + scip.getSolOrigObj(allsols[s]));
+
+      // release variables (if not needed anymore)
+      scip.releaseVar(y);
+      scip.releaseVar(x);
+     
+      // free SCIP
+      scip.free();
+   }
+}
---
+++ examples/Read.java
@@ -0,0 +1,39 @@
+import jscip.*;
+
+/** Example how to read an instance and solve it with the SCIP Java interface. */
+public class Read
+{
+   public static void main(String args[])
+   {
+      if( args.length == 0 || args.length > 2 )
+      {
+         System.out.println("usage: java Read <input file> <settings file (optional)>");
+	 return;
+      }
+   
+      // load generated C-library
+      System.loadLibrary("jni");
+  
+      Scip scip = new Scip();
+      
+      // set up data structures of SCIP
+      scip.create(args[0]);
+
+      // read problem
+      scip.readProb(args[0]);
+
+      // read settings
+      if( args.length == 2 )
+         scip.readParams(args[1]);
+
+      // solve problem
+      scip.solve();
+      
+      // print statistics and the best found solution (only non-zeros)
+      scip.printStatistics();
+      scip.printBestSol(false);
+
+      // free SCIP
+      scip.free();
+    }
+}
